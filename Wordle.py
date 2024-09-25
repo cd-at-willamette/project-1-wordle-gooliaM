55e4ec31cdde4ad8d58cfcb83d14d55471755cb4
gooliaM <jamartin@willamette.edu>
2024-09-24 22:26:50 -0700

    Update 1 Wordle.py
    Milestone 0 - "Display a word"

--- Wordle.py
+++ Wordle.py
@@ -1,8 +1,8 @@
 ########################################
-# Name:
-# Collaborators (if any):
-# GenAI Transcript (if any):
-# Estimated time spent (hr):
+# Name: Julia Martin
+# Collaborators (if any): ChatGPT
+# GenAI Transcript (if any): I used ChatGPT a lot to understand how it worked (I still don't really)- the transcript is long so I can email it if you ask
+# Estimated time spent (hr): 2.5
 # Description of any added extensions:
 ########################################
 
@@ -10,17 +10,24 @@ from WordleGraphics import *  # WordleGWindow, N_ROWS, N_COLS, CORRECT_COLOR, PR
 from english import * # ENGLISH_WORDS, is_english_word
 import random
 
+
 def wordle():
-    # The main function to play the Wordle game.
-
-    def enter_action():
-        # What should happen when RETURN/ENTER is pressed.
-        gw.show_message("You need to implement this method")
-
-
+    
+    #creates a function which iterates through each letter in a word given in the input to 'set' it to the game
+    def display_word(window, word, row):
+        #checks that the word is 5 chars
+        if len(word) == 5:
+            #col was defined with N_COLS = 5, so it knows to loop 5 times
+            #how does letter work? how is it assigned the character value in the string? I can't find anything defining it in the graphics code
+            for col, letter in enumerate(word):
+                #something that places a predefined letter into a specified coordinate
+                window.set_square_letter(row, col, letter)
+    
+    #the name of the window is 'gw', and it's calling the function? class? idk what it is? from the source to run everything
     gw = WordleGWindow()
-    gw.add_enter_listener(enter_action)
-
+    
+    #calling the display_word function with syntax (windowName, word, row#)
+    display_word(gw, "HELLO", 0)
 
 
 
